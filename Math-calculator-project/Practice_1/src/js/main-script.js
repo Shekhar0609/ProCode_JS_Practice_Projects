714988402757789ad391b50d8e3f40e0783890c3
Shekhar0609 <shekhar.deekonda0609@gmail.com>
2025-04-24 11:43:15 +0530

    feat: added the functionality to calculate the value in math-calculator project practice file 1.

--- Math-calculator-project/Practice_1/src/js/main-script.js
+++ Math-calculator-project/Practice_1/src/js/main-script.js
@@ -1,18 +1,25 @@
 const inputElement = document.querySelector("#input");
-
 const ButtonElements = document.querySelectorAll("#button");
 
 let inputValue = "";
 
 ButtonElements.forEach((buttonEl) => {
   buttonEl.addEventListener("click", (e) => {
-    console.log(buttonEl.textContent);
-
-    if (buttonEl.textContent === "AC") {
+    if (e.target.textContent.trim() === "AC") {
       inputValue = "";
-      inputElement.value = "";
+      inputElement.value = inputValue;
+    } else if (e.target.textContent.trim() === "=") {
+      inputValue = eval(inputValue);
+      inputElement.value = inputValue;
+    } else if (e.target.textContent.trim() === "DEL") {
+      inputValue = inputValue.slice(0, -1);
+      inputElement.value = inputValue;
+    } else if (e.target.textContent.trim() === "%") {
+      inputValue = eval(inputValue) / 100;
+      inputElement.value = inputValue;
+    } else {
+      inputValue += e.target.textContent.trim();
+      inputElement.value = inputValue;
     }
-
-    
   });
 });
