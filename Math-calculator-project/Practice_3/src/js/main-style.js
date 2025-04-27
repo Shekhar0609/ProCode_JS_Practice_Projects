a4df3f52f20895ecfd620a15710c951102b5152c
Shekhar0609 <shekhar.deekonda0609@gmail.com>
2025-04-27 22:00:27 +0530

    feat: added the basic functionality to calculate the value in math-calculator project practice file 3.

--- Math-calculator-project/Practice_3/src/js/main-style.js
+++ Math-calculator-project/Practice_3/src/js/main-style.js
@@ -1,12 +1,11 @@
 const displayEL = document.querySelector("#display");
 const buttons = document.querySelectorAll("#button");
 
-let displayValue = "0";
+let displayValue = "";
 
 buttons.forEach((button) => {
   button.addEventListener("click", (e) => {
     const buttonValue = e.target.innerText;
-    console.log(buttonValue);
     // debugger;
     if (buttonValue === "AC") {
       displayValue = "0";
@@ -14,17 +13,29 @@ buttons.forEach((button) => {
     } else if (buttonValue === "DEL") {
       displayValue = displayValue.slice(0, -1);
       displayEL.value = displayValue;
+    } else if (buttonValue === "%") {
+      displayValue = (parseFloat(displayValue) / 100).toString();
+      displayEL.value = displayValue;
     } else if (buttonValue === "=") {
+      if (displayValue === undefined || displayValue === "") {
+        displayValue = "0";
+        displayEL.value = displayValue;
+        return;
+      }
       try {
         displayValue = eval(displayValue);
+        displayEL.value = displayValue;
       } catch (error) {
         displayValue = "Error";
+        displayEL.value = displayValue;
         setTimeout(() => {
-          displayValue = "0";
+          displayValue = "";
           displayEL.value = displayValue;
         }, 1000);
       }
+    } else {
+      displayValue += buttonValue;
+      displayEL.value = displayValue;
     }
-    displayEL.value = displayValue;
   });
 });
