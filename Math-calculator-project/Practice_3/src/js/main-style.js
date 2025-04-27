31e1bd411dcee1658807c9c0a154ab033c629a74
Shekhar0609 <shekhar.deekonda0609@gmail.com>
2025-04-27 16:59:48 +0530

    feat: working on the functionality to calculate the value in math-calculator project practice file 3.

--- Math-calculator-project/Practice_3/src/js/main-style.js
+++ Math-calculator-project/Practice_3/src/js/main-style.js
@@ -1,4 +1,4 @@
-const display = document.querySelector("#display");
+const displayEL = document.querySelector("#display");
 const buttons = document.querySelectorAll("#button");
 
 let displayValue = "0";
@@ -6,23 +6,25 @@ let displayValue = "0";
 buttons.forEach((button) => {
   button.addEventListener("click", (e) => {
     const buttonValue = e.target.innerText;
+    console.log(buttonValue);
+    // debugger;
     if (buttonValue === "AC") {
       displayValue = "0";
-      display.textContent = displayValue;
+      displayEL.value = displayValue;
+    } else if (buttonValue === "DEL") {
+      displayValue = displayValue.slice(0, -1);
+      displayEL.value = displayValue;
     } else if (buttonValue === "=") {
       try {
         displayValue = eval(displayValue);
       } catch (error) {
         displayValue = "Error";
-      }
-    } else {
-      if (displayValue === "0") {
-        displayValue = buttonValue;
-      } else {
-        displayValue += buttonValue;
+        setTimeout(() => {
+          displayValue = "0";
+          displayEL.value = displayValue;
+        }, 1000);
       }
     }
-    display.innerText = displayValue;
-    display.scrollTop = display.scrollHeight; // Scroll to the bottom of the display
+    displayEL.value = displayValue;
   });
 });
