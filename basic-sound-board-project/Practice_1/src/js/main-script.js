80bd6c3b790b73b402e6c7720a9d068645c345d1
Shekhar0609 <shekhar.deekonda0609@gmail.com>
2025-05-20 21:59:26 +0530

    feat: added the stop functionality during the clickin on music for basic sound board practice 1 file.

--- basic-sound-board-project/Practice_1/src/js/main-script.js
+++ basic-sound-board-project/Practice_1/src/js/main-script.js
@@ -1,10 +1,10 @@
 const buttonEls = document.querySelectorAll("button"); // Select all button elements
+let audioEl = document.createElement("audio"); // Create a new audio element
 
 buttonEls.forEach((button) => {
   // Loop through each button
   button.addEventListener("click", () => {
     // Add click event listener to each button
-    let audioEl = document.createElement("audio"); // Create a new audio element
     audioEl.setAttribute(
       // Set the audio element's attributes
       "src", // Set the source of the audio element
@@ -12,10 +12,20 @@ buttonEls.forEach((button) => {
     );
     button.appendChild(audioEl); // Append the audio element to the button
     audioEl.play(); // Play the audio
-
+    stopAudio(); // Call the stopAudio function to stop any currently playing audio
     audioEl.addEventListener("ended", () => {
-      // Add an event listener to remove the audio element when it ends
-      button.removeChild(audioEl); // Remove the audio element from the button
+      // Add event listener for when the audio ends
+      audioEl.remove(); // Remove the audio element from the button
     });
   });
 });
+
+function stopAudio() {
+  // Function to stop audio
+  console.log(audioEl.paused); // Log the paused state of the audio element
+  if (audioEl.paused) {
+    // Check if the audio is paused
+    audioEl.pause(); // Pause the audio if it's playing
+    audioEl.currentTime = 0; // Reset the current time to 0
+  }
+}
