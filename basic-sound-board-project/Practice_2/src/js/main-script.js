8e6335cbc14e0066ebd8568f0dc003d793cd55d7
Shekhar0609 <shekhar.deekonda0609@gmail.com>
2025-05-26 22:10:02 +0530

    feat: stop and start functionality added in the basic sound boad project practice 2 file.

--- basic-sound-board-project/Practice_2/src/js/main-script.js
+++ basic-sound-board-project/Practice_2/src/js/main-script.js
@@ -3,13 +3,33 @@ const buttonEls = document.querySelectorAll("button");
 
 buttonEls.forEach((buttonEle) => {
   buttonEle.addEventListener("click", () => {
+    // debugger;
+    if (
+      buttonEle.children.length > 0 &&
+      buttonEle.children[0].tagName === "AUDIO"
+    ) {
+      // If audio is already playing, stop it
+      stopAudio();
+      return;
+    }
+
     const audioEl = document.createElement("audio");
-    audioEl.setAttribute("src",`./audios/${buttonEle.innerText.toLowerCase()}.mp3`);
-    buttonEle.append(audioEl.src);
+    audioEl.setAttribute(
+      "src",
+      `./audios/${buttonEle.innerText.toLowerCase()}.mp3`
+    );
+    buttonEle.append(audioEl);
     audioEl.play();
 
     audioEl.addEventListener("ended", () => {
-      audioEl.remove();
+      stopAudio();
     });
   });
 });
+
+function stopAudio() {
+  const audioEl = document.querySelector("audio");
+  if (audioEl) {
+    audioEl.remove();
+  }
+}
