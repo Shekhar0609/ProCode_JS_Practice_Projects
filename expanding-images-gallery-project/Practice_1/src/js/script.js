67d0e4e702bf978aad4a8eb0c535d34595c753ac
Shekhar0609 <shekhar.deekonda0609@gmail.com>
2025-06-02 22:14:32 +0530

    added the exapnding funtionality in the expanding image gallery project of practice 1 folder

--- expanding-images-gallery-project/Practice_1/src/js/script.js
+++ expanding-images-gallery-project/Practice_1/src/js/script.js
@@ -1,2 +1,19 @@
-const ImageContainer = document.querySelector('#image-container');
-console.log(ImageContainer);
+const ImageContainer = document.querySelectorAll("#image-container"); // Select all elements with the ID 'image-container'
+
+ImageContainer.forEach((container) => {
+  // Iterate over each container
+  container.addEventListener("click", (evt) => {
+    // Add click event listener to each container
+    evt.stopPropagation(); // Prevent the event from bubbling up to parent elements
+    removeClass(); // Call function to remove 'active-img' class from all containers
+    container.classList.add("active-img"); // Add 'active-img' class to the clicked container
+  });
+});
+
+function removeClass() {
+  // Function to remove 'active-img' class from all containers
+  ImageContainer.forEach((containerEl) => {
+    // Iterate over each container element
+    containerEl.classList.remove("active-img"); // Remove 'active-img' class
+  });
+}
