d78baae3e5f1d1ea7fe092112649a238368a00b8
Shekhar0609 <shekhar.deekonda0609@gmail.com>
2025-06-13 21:13:04 +0530

    added the ripple funtionality in the button ripple effect project of practice 2 folder

--- button-ripple-effect-project/Practice_2/src/js/main-script.js
+++ button-ripple-effect-project/Practice_2/src/js/main-script.js
@@ -1,2 +1,18 @@
 const btnElement = document.querySelector("#btn");
-console.log(btnElement);
+
+btnElement.addEventListener("click", (event) => {
+  let spanElement = document.createElement("span");
+  spanElement.id = "ripple";
+  spanElement.classList = "absolute w-12 h-12 bg-gray-300 rounded-full";
+  btnElement.appendChild(spanElement);
+  let rectangle = btnElement.getBoundingClientRect();
+  let xPosittion = event.clientX - rectangle.left - spanElement.offsetWidth / 2;
+  let yPosittion = event.clientY - rectangle.top - spanElement.offsetWidth / 2;
+  spanElement.style.left = `${xPosittion}px`;
+  spanElement.style.top = `${yPosittion}px`;
+  spanElement.style.transform = "scale(0)";
+
+  btnElement.addEventListener("animationend", () => {
+    spanElement.remove();
+  });
+});
