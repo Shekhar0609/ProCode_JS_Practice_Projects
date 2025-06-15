8c5982323661a0d6bc990196d0be38ec9af3c5b8
Shekhar0609 <shekhar.deekonda0609@gmail.com>
2025-06-15 16:04:26 +0530

    added the increment counter funtionality in the number card counterproject of practice 1 folder

--- number-card-counter-project/Practice_1/src/js/main-script.js
+++ number-card-counter-project/Practice_1/src/js/main-script.js
@@ -1,2 +1,20 @@
-const counterEl = document.querySelector("#counter");
-console.log(counterEl);
+const counterEl = document.querySelectorAll("#counter");
+
+window.addEventListener("load", () => {
+  counterEl.forEach((el) => {
+    el.textContent = "0";
+    updateCounter();
+    function updateCounter() {
+      let currentNum = +el.textContent;
+      const dataCeil = el.getAttribute("data-ceil");
+      const increment = dataCeil / 15;
+      currentNum = Math.ceil(currentNum + increment);
+      if (currentNum < dataCeil) {
+        el.textContent = `${currentNum}`;
+        setTimeout(updateCounter, 50);
+      } else {
+        el.textContent = dataCeil + "k";
+      }
+    }
+  });
+});
