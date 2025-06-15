f23d9f2a1ff2b0f14d94fdc72cc38d6e1beefc83
Shekhar0609 <shekhar.deekonda0609@gmail.com>
2025-06-15 16:06:33 +0530

    added the increment counter funtionality in the number card counterproject of practice 1 folder

--- number-card-counter-project/Practice_1/src/js/main-script.js
+++ number-card-counter-project/Practice_1/src/js/main-script.js
@@ -1,19 +1,25 @@
-const counterEl = document.querySelectorAll("#counter");
+const counterEl = document.querySelectorAll("#counter"); // Select all elements with id "counter"
 
 window.addEventListener("load", () => {
+  // Wait for the window to load
+
   counterEl.forEach((el) => {
-    el.textContent = "0";
-    updateCounter();
+    // Initialize each counter element
+    el.textContent = "0"; // Set initial text content to "0"
+    updateCounter(); // Start the counter update function
     function updateCounter() {
-      let currentNum = +el.textContent;
-      const dataCeil = el.getAttribute("data-ceil");
-      const increment = dataCeil / 15;
-      currentNum = Math.ceil(currentNum + increment);
+      // Function to update the counter
+      let currentNum = +el.textContent; // Convert current text content to a number
+      const dataCeil = el.getAttribute("data-ceil"); // Get the target number from the data attribute
+      const increment = dataCeil / 15; // Calculate the increment value
+      currentNum = Math.ceil(currentNum + increment); // Increment the current number
       if (currentNum < dataCeil) {
-        el.textContent = `${currentNum}`;
-        setTimeout(updateCounter, 50);
+        // Check if the current number is less than the target
+        el.textContent = `${currentNum}`; // Update the text content with the current number
+        setTimeout(updateCounter, 50); // Call the updateCounter function again after 50 milliseconds
       } else {
-        el.textContent = dataCeil + "k";
+        // If the current number has reached or exceeded the target
+        el.textContent = dataCeil + "k"; // Set the text content to the target number with "k"
       }
     }
   });
