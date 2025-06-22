934aef2f2350dbc102b99322b361c7b63da9d427
Shekhar0609 <shekhar.deekonda0609@gmail.com>
2025-06-22 18:39:15 +0530

    added the otp entering and removing functionality in otp verification project practice 2 folder.

--- otp-verification-project/Practice_2/src/js/script.js
+++ otp-verification-project/Practice_2/src/js/script.js
@@ -1,3 +1,36 @@
 const inputElements = document.querySelectorAll("input[type='number']");
+const submitBtn = document.querySelector("#submit-btn");
 
-console.log(inputElements);
+inputElements.forEach(function (element, index) {
+  element.addEventListener("input", (event) => {
+    const currentInput = event.target;
+
+    let value = currentInput.value;
+
+    value = value.replace(/\D/g, "");
+
+    if (value.length > 1) {
+      value = value.slice(0, 1);
+    }
+
+    currentInput.value = value;
+
+    if (index < inputElements.length - 1) {
+      const nextInput = inputElements[index + 1];
+      if (value) {
+        nextInput.focus();
+      } else {
+        currentInput.focus();
+      }
+    }
+
+    element.addEventListener("keydown", (e) => {
+      if (e.key === "Backspace" && element.value === "" && index > 0) {
+        const previousInput = inputElements[index - 1];
+        previousInput.focus();
+      }
+    });
+
+
+  });
+});
