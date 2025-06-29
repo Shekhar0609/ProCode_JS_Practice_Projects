756801f022b42f4a9c663dfcc867cfac802127f6
Shekhar0609 <shekhar.deekonda0609@gmail.com>
2025-06-29 22:03:32 +0530

    added the BMI formuula in js file with comment.

--- BMI-calculator-project/Practice_1/src/js/script.js
+++ BMI-calculator-project/Practice_1/src/js/script.js
@@ -1,3 +1,8 @@
 const heightInCm = document.querySelector("#height-in-cm");
 const weightInKg = document.querySelector("#weight-in-kg");
 console.log(heightInCm, weightInKg);
+
+/*both formula same
+BMI = weight (kg) / (height (m))^2
+BMI = weight (kg) / (height (m) * height (m))
+*/
