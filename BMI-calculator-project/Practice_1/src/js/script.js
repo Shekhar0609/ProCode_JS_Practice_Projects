c252ded6e8c8524ce336a5b3699339aadbc69eb1
Shekhar0609 <shekhar.deekonda0609@gmail.com>
2025-06-30 12:30:46 +0530

    added the BMI calculate funtionality in the BMI calculator project practice 1 folder.

--- BMI-calculator-project/Practice_1/src/js/script.js
+++ BMI-calculator-project/Practice_1/src/js/script.js
@@ -1,8 +1,29 @@
 const heightInCm = document.querySelector("#height-in-cm");
 const weightInKg = document.querySelector("#weight-in-kg");
-console.log(heightInCm, weightInKg);
+const calculateBmiBtn = document.querySelector("#calculate-bmi-btn");
+const resultBmi = document.querySelector("#bmi-result");
 
 /*both formula same
+meter=170 cm / 100 = 1.7 m
 BMI = weight (kg) / (height (m))^2
 BMI = weight (kg) / (height (m) * height (m))
 */
+
+calculateBmiBtn.addEventListener("click", () => {
+  let heightInput = heightInCm.value;
+  let weigthInput = weightInKg.value;
+
+  if (heightInCm.value === "" || weightInKg.value === "") {
+    heightInCm.focus();
+    resultBmi.innerText = "";
+    return alert(`Please enter the input field`);
+  }
+
+  let kgToMeter = heightInput / 100;
+
+  //   BMI = weight (kg) / (height (m) * height (m))
+  let BMI = weigthInput / (kgToMeter * kgToMeter);
+  console.log(BMI);
+
+  resultBmi.innerText = `Your BMI is : ${BMI.toFixed(2)} and Your are Normal`;
+});
